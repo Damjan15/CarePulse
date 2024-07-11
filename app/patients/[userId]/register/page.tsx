b027c5379059810aff7148c531b16f683652258c
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-11 23:57:53 +0200

    feat: user interface finished for the registration page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,8 +3,10 @@ import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Register({ params: { userId } }: SearchParamProps) {
-    const user = await getUser(userId);
+export default async function Register({
+  params: { userId },
+}: SearchParamProps) {
+  const user = await getUser(userId);
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
@@ -19,15 +21,7 @@ export default async function Register({ params: { userId } }: SearchParamProps)
 
           <RegisterForm user={user} />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              &copy; 2024 CarePulse
-            </p>
-
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
+          <p className="copyright py-12">&copy; 2024 CarePulse</p>
         </div>
       </section>
 
