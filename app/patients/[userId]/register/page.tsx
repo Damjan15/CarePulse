8cb98308dd89eb55c8d5e5bccb05c0edaffd3a6f
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-11 08:48:21 +0200

    feat: new RegisterForm component created, new getUser action created

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,7 +1,10 @@
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Register() {
+export default async function Register({ params: { userId } }: SearchParamProps) {
+    const user = await getUser(userId);
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
@@ -13,6 +16,8 @@ export default function Register() {
             width={1000}
             className="mb-12 h-10 w-fit"
           />
+
+          <RegisterForm user={user} />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
