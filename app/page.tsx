27981b75cf83d6281690a3b313cabb5bf0aa2280
Damjan15 <stamenovdamjan5@gmail.com>
2024-07-13 15:10:46 +0200

    feat: PassKeyModal created, functionality for accessing admin dashboard created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
   return (
     <div className="flex h-screen max-h-screen">
+      { isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image src="/assets/icons/logo-full.svg" alt="patient" height={1000} width={1000} className="mb-12 h-10 w-fit" />
